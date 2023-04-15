db08e3466b47896e4230c0c6a80cc4113b9486e7
Vitaliy <vitaliy.ing-el@mail.ru>
2023-04-15 03:08:36 +0300

    all three builds added with build all

--- CoreApp/main.cpp
+++ CoreApp/main.cpp
@@ -1,5 +1,18 @@
 #include "main.hpp"
+#include "stdint.h"
 #include "LowLevel.h"
+
+#ifdef TARGET_POWERSTEP
+volatile uint8_t TestArrayG431[32];
+#endif
+
+#ifdef TARGET_MOSFETS
+volatile uint8_t TestArrayMosfets[32];
+#endif
+
+#ifdef TARGET_F429
+volatile uint8_t TestArrayF429[32];
+#endif
 
 int main(void)
 {
